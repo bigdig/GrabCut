79540e982f8b773807b92f3143520181eb095031
yifanshen <shensahib@gmail.com>
2013-06-11 23:32:47 -0700

    Initial Commit

---
+++ GrabCut/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GrabCut
+//
+//  Created by Yifan Shen on 6/11/13.
+//  Copyright (c) 2013 Yifan Shen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
